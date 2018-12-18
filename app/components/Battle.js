392cea47d6ac2a80bc30a3cb7ccf03a3185329f7
Cody Mac <nicoaudy1994@gmail.com>
2018-12-18 22:57:37 +0700

    player input component and action to parent component

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -1,11 +1,51 @@
 var React = require('react');
+var PlayerInput = require('./PlayerInput');
 var Link = require('react-router-dom').Link;
 
 class Battle extends React.Component {
+	constructor(props){
+		super(props);
+
+		this.state = {
+			playerOneName: '',
+			playerTwoName: '',
+			playerOneImage: null,
+			playerTwoImage: null
+		}
+
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+	handleSubmit(id, username) {
+		this.setState(function(){
+			var newState = {};
+			newState[id + 'Name'] = username;
+			newState[id + 'Image'] = 'https://github.com/' + username + '.png?size=200';
+			return newState;
+		});
+	}
 	render(){
+		var playerOneName = this.state.playerOneName;
+		var playerTwoName = this.state.playerTwoName;
+
 		return (
-			<div className="home-container">
-				Battle
+			<div>
+				<div className="row">
+					{!playerOneName && 
+						<PlayerInput 
+							id="playerOne"
+							label="Player One"
+							onSubmit={this.handleSubmit}
+						/>
+					}
+
+					{!playerTwoName && 
+						<PlayerInput 
+							id="playerTwo"
+							label="Player Two"
+							onSubmit={this.handleSubmit}
+						/>
+					}
+				</div>
 			</div>
 		)	
 	}
